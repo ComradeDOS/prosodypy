422e53acc12a00ea737713e79ebf076cfbdda6b4
Binary <binary@jrudevels.org>
2016-01-02 16:41:17 +0200

    add __main__.py to use "python -m" without warning

---
+++ prosodypy/__main__.py
@@ -0,0 +1,41 @@
+import sys
+import importlib
+
+import lupa
+from lupa import LuaRuntime
+
+lua = LuaRuntime(unpack_returned_tuples=True)
+
+def execute_lua_file(filename):
+    with open(filename, 'rb') as lua_file:
+        code = lua_file.read()
+    if code.startswith('#!'):
+        code = code[code.find('\n'):]
+    lua.execute(code)
+
+def load_code_factory(orig_load_code):
+    def load_code(plugin, resource, env):
+        if plugin.startswith('!py:'):
+            plugin = importlib.import_module(plugin[4:]).ProsodyPlugin(env, lua)
+            return plugin, ""
+        else:
+            return orig_load_code(plugin, resource, env)
+    return load_code
+
+lua_globs = lua.globals()
+lua_globs.arg = lua.table_from(sys.argv[1:])
+lua_globs.load_code_factory = load_code_factory
+
+orig_lua_select = lua_globs.select
+lua.eval('''
+function(load_code_factory, orig_lua_select)
+    _G.select = function(...)
+        local pluginloader = require "util.pluginloader";
+        pluginloader.load_code = load_code_factory(pluginloader.load_code);
+        _G.select = orig_lua_select;
+        return _G.select(...);
+    end
+end
+''')(load_code_factory, orig_lua_select)
+
+execute_lua_file('/usr/bin/prosody') # TODO: don't hardcode
