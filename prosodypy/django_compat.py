a0290f4446264b959c228d9271b70fbc52c6e162
Binary <binary@jrudevels.org>
2016-01-11 16:44:19 +0200

    a decorator that makes sure django db connection is ready to go

--- prosodypy/django_compat.py
+++ prosodypy/django_compat.py
@@ -1,3 +1,5 @@
+from functools import wraps
+
 from prosodypy import lua
 
 assert lua
@@ -8,3 +10,14 @@ settings_module = config.get("*", "django_settings_module")
 os.environ['DJANGO_SETTINGS_MODULE'] = settings_module
 import django
 django.setup()
+
+from django.db import connections
+
+def db_connect(func, using='default'):
+    @wraps(func)
+    def inner(*args, **kwargs):
+        connection = connections[using]
+        connection.close_if_unusable_or_obsolete()
+        connection.ensure_connection()
+        return func(*args, **kwargs)
+    return inner
