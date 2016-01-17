bb18b2e6d5aab4254ab4db9f530d8a6787692f7e
Binary <binary@jrudevels.org>
2016-01-17 19:43:34 +0200

    building stanzas back to prosody

--- prosodypy/twilix_compat/element.py
+++ prosodypy/twilix_compat/element.py
@@ -1,3 +1,7 @@
+from copy import copy
+
+from prosodypy import lua
+
 from twisted.words.xish.xmlstream import XmlStream
 
 class FakeStream(XmlStream):
@@ -15,9 +19,35 @@ class FakeStream(XmlStream):
 xmlstream = FakeStream()
 
 def parse_string(stanza):
-    xmlstream.dataReceived(stanza)
+    xmlstream.dataReceived(stanza.encode('utf-8'))
     result = xmlstream.resulted_element
     assert result is not None
     xmlstream.resulted_element = None
     return result
 
+def parse_prosody_stanza(stanza):
+    assert lua is not None
+    return parse_string(lua.eval('tostring')[1](stanza))
+
+if lua:
+    Stanza = lua.require("util.stanza").stanza
+def build_prosody_stanza(element, current_ns=None, current_tag=None):
+    name = element.name
+    attrs = copy(element.attributes)
+    if element.uri != current_ns:
+        attrs['xmlns'] = element.uri
+        current_ns = element.uri
+    added_tag = False
+    if current_tag is None:
+        current_tag = Stanza(name, lua.table_from(attrs))
+    else:
+        current_tag.tag(current_tag, name, lua.table_from(attrs))
+        added_tag = True
+    for child in element.children:
+        if isinstance(child, unicode):
+            current_tag.text(current_tag, child)
+        else:
+            build_prosody_stanza(child, current_ns, current_tag)
+    if added_tag:
+        current_tag.up(current_tag)
+    return current_tag
